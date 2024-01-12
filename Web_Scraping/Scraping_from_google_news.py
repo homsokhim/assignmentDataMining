5b997622b266a0b90230d753bb9582581392cf67
sokhimhom168 <sokhimhom168@gmail.com>
2024-01-13 00:36:50 +0700

    update dataset and do new classifier with new dataset.

--- Web_Scraping/Scraping_from_google_news.py
+++ Web_Scraping/Scraping_from_google_news.py
@@ -3,11 +3,11 @@ import requests
 from bs4 import BeautifulSoup
 
 # Send a GET request to the URL
-# url = 'https://edition.cnn.com/business'
-# url = 'https://edition.cnn.com/politics'
-# url = 'https://edition.cnn.com/world'
-# url = 'https://edition.cnn.com/health'
-url = 'https://edition.cnn.com/entertainment'
+# url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen' #world
+#url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen' #Business
+#url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen' #Entertainment
+#url = 'https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen' #Health
+url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp1ZEdvU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen' #sport
 response = requests.get(url)
 response.raise_for_status()
 
@@ -15,10 +15,10 @@ response.raise_for_status()
 soup = BeautifulSoup(response.content, 'html.parser')
 
 # Find all <span> elements with class 'container__headline-text'
-span_elements = soup.find_all('span', class_='container__headline-text')
+span_elements = soup.find_all('a', class_='gPFEn')
 
 # Find all <h1> elements with class 'headline__text'
-h1_elements = soup.find_all('h1', class_='headline__text')
+h1_elements = soup.find_all('h1', class_='BPNpve')
 
 # Create a list to store the extracted data
 data = []
@@ -36,7 +36,7 @@ for h1 in h1_elements:
     data.append({'Title': title, 'Category': category})
 
 # Export the data to a CSV file
-filename = 'cnn_world_news_entertainment.csv'
+filename = 'google_world_news_Sports.csv'
 fields = ['Title', 'Category']
 
 with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
